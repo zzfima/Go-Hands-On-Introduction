596fb7e84a763828c6a8ed7c200a0d0e27d53a64
Efim Zabarsky <Efim.Zabarsky@kla-tencor.com>
2022-11-28 15:59:38 +0200

    done

--- packages/challenge.go
+++ packages/challenge.go
@@ -2,14 +2,21 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"time"
 
 	proverbs "github.com/jboursiquot/go-proverbs"
 )
 
 func main() {
-	fmt.Println(getRandomProverb())
+	v1 := getRandomProverb()
+	fmt.Println(v1)
+	v2 := getRandomProverb()
+	fmt.Fprint(os.Stdout, v2)
 }
 
 func getRandomProverb() string {
-	return proverbs.Random().Saying
+	p := proverbs.Random()
+	time.Sleep(10)
+	return p.Saying
 }
