fad4bb13baab1f9915969786ad2247b58d3187d2
Efim Zabarsky <Efim.Zabarsky@kla-tencor.com>
2022-12-06 12:22:22 +0200

    done challenge 4: flow control

---
+++ flow_control/challenge.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"unicode"
+
+	"github.com/davecgh/go-spew/spew"
+)
+
+// run as "go run challenge.go data.txt"
+// or add to run configuration: "args": ["data.txt"]
+func main() {
+	//defers
+	defer func() {
+		if e := recover(); e != nil {
+			fmt.Println("Panic!!!! :-( ", e)
+		} else {
+			fmt.Println("no panic :-)")
+		}
+	}()
+
+	b, e := os.ReadFile(os.Args[1])
+	if e != nil {
+		fmt.Println(e)
+		return
+	}
+
+	textMap := make(map[string]int)
+	words := strings.Split(string(b), " ")
+	textMap["words"] = len(words)
+
+	for _, c := range b {
+		r := int32(c)
+
+		switch {
+		case unicode.IsSpace(r):
+			textMap["spaces"]++
+		case unicode.IsLetter(r):
+			textMap["letters"]++
+		case unicode.IsDigit(r):
+			textMap["numbers"]++
+		default:
+			textMap["symbols"]++
+		}
+	}
+
+	spew.Dump(textMap)
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/zzfima/Go-Hands-On-Introduction
 
 go 1.19
 
-require github.com/jboursiquot/go-proverbs v0.0.2
+require (
+	github.com/davecgh/go-spew v1.1.1
+	github.com/jboursiquot/go-proverbs v0.0.2
+)
