2b69dc021b7c0f4a1343049f3f708e03e38d5b3a
Efim Zabarsky <Efim.Zabarsky@kla-tencor.com>
2022-12-19 15:35:06 +0200

    add benchmarks

--- testing/logic_benchmark_test.go
+++ testing/logic_benchmark_test.go
@@ -4,9 +4,23 @@ import (
 	"testing"
 )
 
-// BenchmarkAddElementsArray ...
-func BenchmarkAddElementsArray(b *testing.B) {
+// BenchmarkAddElementsSlice ...
+func BenchmarkAddElementsSlice1(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		sumElementsSlice(1, 2, 3)
+	}
+}
+
+func BenchmarkAddElementsSlice2(b *testing.B) {
+	nums := []int{1, 2, 3}
+	for i := 0; i < b.N; i++ {
+		sumElementsSlice(nums...)
+	}
+}
+
+func BenchmarkAddElementsArray(b *testing.B) {
+	nums := []int{1, 2, 3}
+	for i := 0; i < b.N; i++ {
+		sumElementsArray(nums)
 	}
 }
