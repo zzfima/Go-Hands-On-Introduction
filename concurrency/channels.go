c8d61b668b74c0f7a5c11336e51c48f05b68b76f
Efim Zabarsky <Efim.Zabarsky@kla-tencor.com>
2022-12-20 12:58:39 +0200

    rfct

--- concurrency/channels.go
+++ concurrency/channels.go
@@ -6,8 +6,9 @@ import (
 )
 
 func f2(ch chan int) {
-	time.Sleep(50 * time.Millisecond)
+	time.Sleep(500 * time.Millisecond)
 	fmt.Println("i am 2")
+	time.Sleep(120 * time.Millisecond)
 	ch <- 5
 }
 
@@ -15,11 +16,21 @@ func f3(ch chan int) {
 	fmt.Println("i am 3", <-ch)
 }
 
+func f4() {
+	cnt := 0
+	for {
+		cnt++
+		fmt.Println(cnt)
+		time.Sleep(100 * time.Millisecond)
+	}
+}
+
 func mainChannels() {
 	ch := make(chan int)
 
 	go f2(ch)
 	go f3(ch)
+	go f4()
 
-	time.Sleep(100 * time.Millisecond)
+	time.Sleep(1000 * time.Millisecond)
 }
